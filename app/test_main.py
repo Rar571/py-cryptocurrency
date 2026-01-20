2a2f4f35354a69ceabfc443fcb2c7d7c43afb1cd
stadnikruslan168-hub <stadnikruslan168@gmail.com>
2026-01-20 18:24:04 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,12 +2,13 @@ from unittest import mock
 import pytest
 from app.main import cryptocurrency_action
 
+
 @pytest.mark.parametrize(
-     "current_rate, predicted_rate, expected_result",
-     [
-         (100, 105, "Do nothing"),
-         (100, 95, "Do nothing"),
-     ]
+    "current_rate, predicted_rate, expected_result",
+    [
+        (100, 105, "Do nothing"),
+        (100, 95, "Do nothing"),
+    ]
 )
 def test_rate_main(current_rate: int | float,
         predicted_rate: int | float, expected_result: str) -> None:
